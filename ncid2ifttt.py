373ede7a811bd0a9c686d235a4b966d392fe3a0e
gonzotek <gonzotek@gmail.com>
2018-03-16 20:01:58 -0400

    hm...

--- ncid2ifttt.py
+++ ncid2ifttt.py
@@ -38,7 +38,7 @@ def main():
 			if data[:4] == "CID:":
 				nmbr = incomingCall(data[:-1])
 				pyfttt.send_event(ifttt_key, ifttt_event, nmbr)
-				time.sleep(0.05)
+				#time.sleep(0.05)
 	except:
 		pass
 	finally:
